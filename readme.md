3f2963d80e92a500807a1283a17c5c14c9336351
jh_tang <ktangjh@gmail.com>
2019-04-10 09:46:39 +0800

    Updated readme, changed method in generate_data.py
    Updated readme. Changed the method for generating demo data to a more relational structure

--- readme.md
+++ readme.md
@@ -3,11 +3,22 @@
 ### Purpose
 A Python script to scan through a directory recursively for Excel files to be combined. The script assumes all the files to be of the same structure, and concatenates the data into a single Excel file.
 
-### How to Use
-Place the script `master_merge.py` in the root of the target folder and run it. Any files with the extension `.xlsx` will be detected and loaded to a dataframe, which is subsequently combined. 
+### Use Case
+The example given here is the evaluation of a collection of items, which can be products/personnels, distributed to a collection of assessors. Each assessor will submit an Excel scoresheet for their allocated items.
 
-A log file containing information of the import details will be generated after the script is run.
+Each item is assigned three assessors and assessed on three criteria. The script `master_merge.py` is used to concatenate data from every assessor's Excel scoresheet into a single one.
+
+It is possible to include codes to transform and aggregate the data further with `Python` via `pandas` dataframes.
+
+### How to Use
+* Place the script `master_merge.py` in the root of the target folder and run it.
+	* Any files with the extension `.xlsx` will be detected and loaded to a dataframe, which is subsequently combined. 
+	* The script scans for files at the same level of its location and any child folders.
+	
+* You can run `generate_data.py` to create a demo folder structure for testing purpose.
+
+* Once run, a log file containing information of the import details will be generated and a `master_table.xlsx` will be created
 
 #### Note
-* A windows `merge.bat` file is included to facilitate running of the script. Ensure that Python and the libraries are added to the environment path for it to work.
-* A script `generate_data.py` is included to generate dummy data for test purpose
+* A windows `merge.bat` file is included to facilitate running of the script. Ensure that `Python` and the libraries are added to the environment path for it to work.
+* This method bypasses the locked single-instance of MS Office applications, allowing for data to be grabbed even when files are opened
--- src/generate_data.py
+++ src/generate_data.py
@@ -1,39 +1,67 @@
+'''
+Module to generate a demo folder structure for testing purpose.
+This demo takes the structure of individual Excel files based on an assessor of the test candidate.
+Each candidate is assigned 3 assessors and will be assessed on 3 criteria
+'''
+
 # Import libraries
 import os
 import numpy as np
 import pandas as pd
-import string
 
-# Function to generate demo folder tree and files
-def generate_rand_df():
-    names = []
-    scores_1 = []
-    scores_2 = []
+# Function to generate a list of unique running ID
+def generate_ids(prefix="ID", suffix="", num=480):
+    ids = []
+    for i in range(num):
+        ids.append('{}{}{}'.format(prefix, str(i).zfill(5), suffix))
+    return ids
+
+# Generate an allocation table of assessor to candidate
+cohort = generate_ids()
+assessor = generate_ids(prefix="Ax", suffix="N", num=50)
+asc_1 = []
+asc_2 = []
+asc_3 = []
+for c in cohort:
+    first, second, third = (np.random.choice(assessor) for i in range(3))
+    while second == first:
+        second = np.random.choice(assessor)
+    while third == second or third == first:
+        third = np.random.choice(assessor)
+    asc_1.append(first)
+    asc_2.append(second)
+    asc_3.append(third)
+    
+data = list(zip(cohort, asc_1, asc_2, asc_3))
+df_allocation = pd.DataFrame(data, columns=['Candidate','Asc1','Asc2','Asc3'])
+
+# Creates a 2>>4 folder structure and randomly allocate assessors dataframe as Excel file
+subdir_1 = ['Group_1', 'Group_2']
+subdir_2 = ['Section_1', 'Section_2', 'Section_3', 'Section_4']
+new_paths = []
+for i in subdir_1:
+    for j in subdir_2:
+        new_paths.append('proj_root/{}/{}/'.format(i,j))
+        for n in new_paths:
+            os.makedirs(n, mode=777, exist_ok=True)
+            
+# Generate collection of dataframes based on assessors with random scores and randomly allocate to subfolders
+asc_total = np.unique(asc_1 + asc_2 + asc_3) #ensure all unique assessors included
+frames = []
+for a in asc_total:
+    ids_cnd = []
+    score_1 = []
+    score_2 = []
+    score_3 = []
     data = []
-    for n in range(20):
-        names.append(''.join(np.random.choice(list(string.ascii_letters), size=8)))
-        scores_1.append(np.random.randint(1,10))
-        scores_2.append(np.random.randint(1,10))
-    data = list(zip(names,scores_1,scores_2))
-    return pd.DataFrame(data, columns = ['Name', 'Score_1', 'Score_2'])
-
-# Function to generate dummy data with specified folder tree
-def folder_str(subdir_one, subdir_two, files, root_path="proj_root"):
-    ''' 
-    files = number of files within each second level subfolders
-    subdir_one = number of first level subfolders
-    subdir_two = number of second level subfolders
-    root_path = path name of root folder
-    '''
-    new_paths = []
-    for i in range(subdir_one):
-        for j in range(subdir_two):
-            new_paths.append("{}/folder{}/subfolder{}".format(root_path,i,j))
-            for n in new_paths:
-                os.makedirs(n, mode=777, exist_ok=True)
-                for k in range(files):
-                    generate_rand_df().to_excel('{}/file{}.xlsx'.format(n, k), index=False)
-
-# Generate the dummy folder tree
-# folder_str(2, 2, 2)
-
+    candidates = []
+    candidates = df_allocation['Candidate'][(df_allocation['Asc1']==a)|(df_allocation['Asc2']==a)|(df_allocation['Asc3']==a)]
+    for c in candidates:
+        ids_cnd.append(c)
+        score_1.append(np.random.randint(1,10))
+        score_2.append(np.random.randint(1,10))
+        score_3.append(np.random.randint(1,10))
+    data_a = list(zip(ids_cnd,score_1,score_2,score_3))
+    df = pd.DataFrame(data_a, columns=['Candidates', 'Criterion_1', 'Criterion_2', 'Criterion_3'])
+    frames.append(df)
+    df.to_excel('{}{}.xlsx'.format(np.random.choice(new_paths), a), index=False)
